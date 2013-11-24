caab87dc093288aa5a85db7b1f6abb9c82e490f3
nblumhardt <nblumhardt@nblumhardt.com>
2013-11-24 10:48:32 +1000

    Added JSON formatting test.

---
+++ test/Seq.Client.FullNetFx.Tests/JsonFormatterTests.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.IO;
+using NUnit.Framework;
+using Newtonsoft.Json;
+using Seq.Client.FullNetFx.Tests.Support;
+using Serilog.Events;
+using Serilog.Parsing;
+using System.Linq;
+
+namespace Seq.Client.FullNetFx.Tests
+{
+    [TestFixture]
+    public class JsonFormatterTests
+    {
+        [Test]
+        public void PropertyTokensWithFormatStringsAreIncludedAsRenderings()
+        {
+            var p = new MessageTemplateParser();
+            var e = new LogEvent(Some.OffsetInstant(), LogEventLevel.Information, null,
+                p.Parse("{AProperty:000}"), new[] { new LogEventProperty("AProperty", new ScalarValue(12)) });
+
+            var d = FormatEvent(e);
+
+            var rs = ((IEnumerable)d.Renderings).Cast<dynamic>().ToArray();
+            Assert.AreEqual(1, rs.Count());
+            var ap = d.Renderings.AProperty;
+            var fs = ((IEnumerable)ap).Cast<dynamic>().ToArray();
+            Assert.AreEqual(1, fs.Count());
+            Assert.AreEqual("000", (string)fs.Single().Format);
+            Assert.AreEqual("012", (string)fs.Single().Rendering);
+        }
+
+        static dynamic FormatEvent(LogEvent e)
+        {
+            var j = new JsonFormatter();
+
+            var f = new StringWriter();
+            j.Format(e, f);
+
+            var d = JsonConvert.DeserializeObject<dynamic>(f.ToString());
+            return d;
+        }
+
+        [Test]
+        public void PropertyTokensWithoutFormatStringsAreNotIncludedAsRenderings()
+        {
+            var p = new MessageTemplateParser();
+            var e = new LogEvent(Some.OffsetInstant(), LogEventLevel.Information, null,
+                p.Parse("{AProperty}"), new[] { new LogEventProperty("AProperty", new ScalarValue(12)) });
+
+            var d = FormatEvent(e);
+
+            var rs = ((IEnumerable)d.Renderings).Cast<dynamic>().ToArray();
+            Assert.AreEqual(0, rs.Count());
+        }
+    }
+}
---
+++ test/Seq.Client.FullNetFx.Tests/Support/Some.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Threading;
+
+namespace Seq.Client.FullNetFx.Tests.Support
+{
+    static class Some
+    {
+        static int Counter;
+
+        public static int Int()
+        {
+            return Interlocked.Increment(ref Counter);
+        }
+
+        public static string String(string tag = null)
+        {
+            return (tag ?? "") + "__" + Int();
+        }
+
+        public static TimeSpan TimeSpan()
+        {
+            return System.TimeSpan.FromMinutes(Int());
+        }
+
+        public static DateTime Instant()
+        {
+            return new DateTime(2012, 10, 28) + TimeSpan();
+        }
+
+        public static DateTimeOffset OffsetInstant()
+        {
+            return new DateTimeOffset(Instant());
+        }
+    }
+}
