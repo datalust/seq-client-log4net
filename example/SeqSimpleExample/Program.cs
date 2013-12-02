e31cfb79aacbd5b8e2dbc68e3bf056d2ed3ce37d
nblumhardt <nblumhardt@nblumhardt.com>
2013-12-03 06:22:52 +1000

    Dropped in the super simple example from the introductory post.

---
+++ example/SeqSimpleExample/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Seq;
+using Serilog;
+
+namespace SeqSimpleExample
+{
+    class Program
+    {
+        public static void Main()
+        {
+            Log.Logger = new LoggerConfiguration()
+                .WriteTo.ColoredConsole()
+                .WriteTo.Seq("http://localhost:5341")
+                .CreateLogger();
+
+            Log.Information("Hello, {Name}!", Environment.UserName);
+            Console.ReadKey(true);
+        }
+    }
+}
