7af2e4016b5b60b3a9c98fc24ecd83f6196f2908
nblumhardt <nblumhardt@nblumhardt.com>
2014-02-15 20:29:05 +1000

    NLog target.

---
+++ src/Seq.Client.NLog/Client/NLog/LogEventInfoFormatter.cs
@@ -0,0 +1,256 @@
+﻿// Seq Client for .NET - Copyright 2013 Continuous IT Pty Ltd
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//    http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Text;
+using NLog;
+
+namespace Seq.Client.NLog
+{
+    static class LogEventInfoFormatter
+    {
+        static readonly IDictionary<Type, Action<object, TextWriter>> _literalWriters;
+
+        static LogEventInfoFormatter()
+        {
+            _literalWriters = new Dictionary<Type, Action<object, TextWriter>>
+            {
+                { typeof(byte), WriteToString },
+                { typeof(sbyte), WriteToString },
+                { typeof(short), WriteToString },
+                { typeof(ushort), WriteToString },
+                { typeof(int), WriteToString },
+                { typeof(uint), WriteToString },
+                { typeof(long), WriteToString },
+                { typeof(ulong), WriteToString },
+                { typeof(float), WriteToString },
+                { typeof(double), WriteToString },
+                { typeof(decimal), WriteToString },
+                { typeof(string), (v, w) => WriteString((string)v, w) },
+                { typeof(DateTime), (v, w) => WriteDateTime((DateTime)v, w) },
+                { typeof(DateTimeOffset), (v, w) => WriteOffset((DateTimeOffset)v, w) },
+            };
+        }
+
+        static readonly IDictionary<string, string> _levelMap = new Dictionary<string, string>
+        {
+            { "Trace", "Verbose" },
+            { "Debug", "Debug" },
+            { "Info", "Information" },
+            { "Warn", "Warning" },
+            { "Error", "Error" },
+            { "Fatal", "Fatal" }
+        };
+
+        public static void ToJson(IEnumerable<LogEventInfo> events, StringWriter payload)
+        {
+            var delim = "";
+            foreach (var loggingEvent in events)
+            {
+                payload.Write(delim);
+                delim = ",";
+                ToJson(loggingEvent, payload);
+            }
+        }
+
+        static void ToJson(LogEventInfo loggingEvent, StringWriter payload)
+        {
+            string level;
+            if (!_levelMap.TryGetValue(loggingEvent.Level.Name, out level))
+                level = "Information";
+
+            payload.Write("{");
+
+            var delim = "";
+            var offsetTimestamp = new DateTimeOffset(loggingEvent.TimeStamp, DateTimeOffset.Now.Offset);
+            WriteJsonProperty("Timestamp", offsetTimestamp, ref delim, payload);
+            WriteJsonProperty("Level", level, ref delim, payload);
+
+            WriteJsonProperty("MessageTemplate", loggingEvent.Message, ref delim, payload);
+
+            if (loggingEvent.Exception != null)
+                WriteJsonProperty("Exception", loggingEvent.Exception, ref delim, payload);
+
+            payload.Write(",\"Properties\":{");
+
+            var pdelim = "";
+
+            if (loggingEvent.Parameters != null)
+            {
+                for (var i = 0; i < loggingEvent.Parameters.Length; ++i)
+                {
+                    WriteJsonProperty(i.ToString(CultureInfo.InvariantCulture), loggingEvent.Parameters[i], ref pdelim, payload);
+                }
+            }
+
+            if (loggingEvent.Properties != null)
+            {
+                var seenKeys = new HashSet<string>();
+                foreach (var property in loggingEvent.Properties)
+                {
+                    var sanitizedKey = SanitizeKey(property.Key.ToString());
+                    if (seenKeys.Contains(sanitizedKey))
+                        continue;
+
+                    seenKeys.Add(sanitizedKey);
+                    WriteJsonProperty(sanitizedKey, property.Value, ref pdelim, payload);
+                }
+            }
+
+            payload.Write("}");
+            payload.Write("}");
+        }
+
+        static string SanitizeKey(string key)
+        {
+            return new string(key.Replace(":", "_").Where(c => c == '_' || char.IsLetterOrDigit(c)).ToArray());
+        }
+
+
+        static void WriteJsonProperty(string name, object value, ref string precedingDelimiter, TextWriter output)
+        {
+            output.Write(precedingDelimiter);
+            WritePropertyName(name, output);
+            WriteLiteral(value, output);
+            precedingDelimiter = ",";
+        }
+
+        static void WritePropertyName(string name, TextWriter output)
+        {
+            output.Write("\"");
+            output.Write(name);
+            output.Write("\":");
+        }
+
+        static void WriteLiteral(object value, TextWriter output)
+        {
+            if (value == null)
+            {
+                output.Write("null");
+                return;
+            }
+
+            Action<object, TextWriter> writer;
+            if (_literalWriters.TryGetValue(value.GetType(), out writer))
+            {
+                writer(value, output);
+                return;
+            }
+
+            WriteString(value.ToString(), output);
+        }
+
+        static void WriteToString(object number, TextWriter output)
+        {
+            output.Write(number.ToString());
+        }
+
+        static void WriteOffset(DateTimeOffset value, TextWriter output)
+        {
+            output.Write("\"");
+            output.Write(value.ToString("o"));
+            output.Write("\"");
+        }
+
+        static void WriteDateTime(DateTime value, TextWriter output)
+        {
+            output.Write("\"");
+            output.Write(value.ToString("o"));
+            output.Write("\"");
+        }
+
+        static void WriteString(string value, TextWriter output)
+        {
+            var content = Escape(value);
+            output.Write("\"");
+            output.Write(content);
+            output.Write("\"");
+        }
+
+        static string Escape(string s)
+        {
+            if (s == null) return null;
+
+            StringBuilder escapedResult = null;
+            var cleanSegmentStart = 0;
+            for (var i = 0; i < s.Length; ++i)
+            {
+                var c = s[i];
+                if (c < (char)32 || c == '\\' || c == '"')
+                {
+
+                    if (escapedResult == null)
+                        escapedResult = new StringBuilder();
+
+                    escapedResult.Append(s.Substring(cleanSegmentStart, i - cleanSegmentStart));
+                    cleanSegmentStart = i + 1;
+
+                    switch (c)
+                    {
+                        case '"':
+                            {
+                                escapedResult.Append("\\\"");
+                                break;
+                            }
+                        case '\\':
+                            {
+                                escapedResult.Append("\\\\");
+                                break;
+                            }
+                        case '\n':
+                            {
+                                escapedResult.Append("\\n");
+                                break;
+                            }
+                        case '\r':
+                            {
+                                escapedResult.Append("\\r");
+                                break;
+                            }
+                        case '\f':
+                            {
+                                escapedResult.Append("\\f");
+                                break;
+                            }
+                        case '\t':
+                            {
+                                escapedResult.Append("\\t");
+                                break;
+                            }
+                        default:
+                            {
+                                escapedResult.Append("\\u");
+                                escapedResult.Append(((int)c).ToString("X4"));
+                                break;
+                            }
+                    }
+                }
+            }
+
+            if (escapedResult != null)
+            {
+                if (cleanSegmentStart != s.Length)
+                    escapedResult.Append(s.Substring(cleanSegmentStart));
+
+                return escapedResult.ToString();
+            }
+
+            return s;
+        }
+    }
+}
---
+++ src/Seq.Client.NLog/Client/NLog/SeqTarget.cs
@@ -0,0 +1,83 @@
+﻿// Seq Client for .NET - Copyright 2013 Continuous IT Pty Ltd
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//    http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.IO;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using NLog;
+using NLog.Common;
+using NLog.Targets;
+
+namespace Seq.Client.NLog
+{
+    /// <summary>
+    /// Writes events over HTTP to a Seq server.
+    /// </summary>
+    [Target("Seq")]
+    public sealed class SeqTarget : Target
+    {
+        const string BulkUploadResource = "/api/events/raw";
+
+        /// <summary>
+        /// The address of the Seq server to write to.
+        /// </summary>
+        [Required]
+        public string ServerUrl { get; set; }
+
+        /// <summary>
+        /// Writes an array of logging events to Seq.
+        /// </summary>
+        /// <param name="logEvents">Logging events to be written.</param>
+        protected override void Write(AsyncLogEventInfo[] logEvents)
+        {
+            var events = logEvents.Select(e => e.LogEvent);
+            PostBatch(events);
+        }
+
+        /// <summary>
+        /// Writes logging event to Seq.
+        /// </summary>
+        /// <param name="logEvent">Logging event to be written.
+        ///             </param>
+        protected override void Write(LogEventInfo logEvent)
+        {
+            PostBatch(new[] { logEvent });
+        }
+
+        void PostBatch(IEnumerable<LogEventInfo> events)
+        {
+            if (ServerUrl == null)
+                return;
+
+            var payload = new StringWriter();
+            payload.Write("{\"events\":[");
+            LogEventInfoFormatter.ToJson(events, payload);
+            payload.Write("]}");
+
+            var content = new StringContent(payload.ToString(), Encoding.UTF8, "application/json");
+
+            using (var httpClient = new HttpClient {BaseAddress = new Uri(ServerUrl)})
+            {
+                var result = httpClient.PostAsync(BulkUploadResource, content).Result;
+                if (!result.IsSuccessStatusCode)
+                    throw new HttpRequestException(string.Format("Received failed result {0}: {1}", result.StatusCode,
+                        result.Content.ReadAsStringAsync().Result));
+            }
+        }
+    }
+}
