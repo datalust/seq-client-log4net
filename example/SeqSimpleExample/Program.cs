4de60c0ef18c6bf458120bedd941757a5a429ea4
nblumhardt <nblumhardt@nblumhardt.com>
2014-02-22 14:12:31 +1000

    Updated Serilog.

--- example/SeqSimpleExample/Program.cs
+++ example/SeqSimpleExample/Program.cs
@@ -1,6 +1,8 @@
 ﻿using System;
+using System.Threading;
 using Seq;
 using Serilog;
+using Serilog.Debugging;
 
 namespace SeqSimpleExample
 {
@@ -8,12 +10,19 @@ namespace SeqSimpleExample
     {
         public static void Main()
         {
+            SelfLog.Out = Console.Out;
+
             Log.Logger = new LoggerConfiguration()
                 .WriteTo.ColoredConsole()
-                .WriteTo.Seq("http://localhost:5341")
+                .WriteTo.Seq("http://my-seq", "Logs\\sample")
                 .CreateLogger();
 
-            Log.Information("Hello, {Name}!", Environment.UserName);
+            for (int i = 0; i < 2000; ++i)
+            {
+                var p = new string('P', i % 200);
+    //            Log.Information("Hello, {Name} {P}!", Environment.UserName, p);
+                Thread.Sleep(10);
+            }
             Console.ReadKey(true);
         }
     }
