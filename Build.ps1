d75bea3be09565bbb984fdc7b9cf570faef21b10
Nicholas Blumhardt <nblumhardt@nblumhardt.com>
2026-02-19 15:07:54 +1000

    Use alternative build sequence to resolve publishing issue

--- Build.ps1
+++ Build.ps1
@@ -28,15 +28,18 @@ try {
     Write-Output "build: Package version suffix is $suffix"
     Write-Output "build: Build version suffix is $buildSuffix"
 
+    & dotnet build -c Release --version-suffix=$buildSuffix /p:ContinuousIntegrationBuild=true
+    if($LASTEXITCODE -ne 0) { throw "Build failed" }
+
     foreach ($src in Get-ChildItem src/*) {
         Push-Location $src
 
         Write-Output "build: Packaging project in $src"
 
         if ($suffix) {
-            & dotnet pack -c Release -o ../../artifacts --version-suffix=$suffix
+            & dotnet pack -c Release --no-build --no-restore  -o ../../artifacts --version-suffix=$suffix
         } else {
-            & dotnet pack -c Release -o ../../artifacts
+            & dotnet pack -c Release --no-build --no-restore  -o ../../artifacts
         }
         if($LASTEXITCODE -ne 0) { throw "Packaging failed" }
 
